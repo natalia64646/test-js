915503b6f51028d0a6802c3bdf97080bacb8191f
natalia64646 <Zayatsstepan7@gmail.com>
2021-08-09 18:09:22 +0000

    findet out age

--- js/hello.js
+++ js/hello.js
@@ -1,28 +1,37 @@
+function findOutAge (){
+    const age =  prompt('how old are you?');
+    const newAge = Number(age) + 1;
+    const endMassegeKids = 'Cool, you will soon ' + newAge;
+    const endMassegeYoung = 'Only ' + age + ', of you are so young!';
+    const endMassegeOld = 'A wonderful age!';
 
-// const age =  prompt('how old are you?');
-// const newAge = Number(age) + 1;
-// const endMassegeKids = 'Cool, you will soon ' + newAge;
-// const endMassegeYoung = 'Only ' + age + ', of you are so young!';
-// const endMassegeOld = 'A wonderful age!';
-
-// if (age > 40){
-// alert(endMassegeOld);
-// }
-// if (40 >= age >= 20){
-// alert (endMassegeYoung);
-// }
-// else{
-// alert (endMassegeKids);
-// }
-
-const year = Number(prompt('Enter year of birth'));
-const age = difference(2021, year);
-function foundAge (a, b){
-    const ageDifference =  a - b;
-    return ageDifference;
+if (age > 40){
+ return alert(endMassegeOld);
+} else if (age <= 40 && age >= 20){
+ return alert (endMassegeYoung);
+} else{
+alert (endMassegeKids);
+};
 }
 
-alert (ageDifference);
+findOutAge();
+
+
+
+
+
+
+
+
+
+// const year = Number(prompt('Enter year of birth'));
+// const age = difference(2021, year);
+// function foundAge (a, b){
+//     const ageDifference =  a - b;
+//     return ageDifference;
+// }
+
+// alert (ageDifference);
 
 
 
